99d962670b80e6cdafc961dcfecc1033fff13404
una <by2live@hotmail.com>
2018-01-18 11:33:57 +0800

    招募字段

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -501,12 +501,14 @@ class UserService{
                     foreach ($vel as $key1 => $vel1){
                         $num += $vel1['number'];
                         $numbers += $vel1['number'];
+                        $result[$k][$ke][$key][$key1]['wechat_id'] = $info[$vel1['target_user_id']]['wechat_id'];
+                        $result[$k][$ke][$key][$key1]['phone'] = $info[$vel1['target_user_id']]['phone'];
                     }
                     $result[$k][$ke][$key]['num'] = $num;
                     $num = 0;
                 }
-                $result[$k][$ke]['wechat_id'] = $info[$vel1['target_user_id']]['wechat_id'];
-                $result[$k][$ke]['phone'] = $info[$vel1['target_user_id']]['phone'];
+//                $result[$k][$ke]['wechat_id'] = $info[$vel1['target_user_id']]['wechat_id'];
+//                $result[$k][$ke]['phone'] = $info[$vel1['target_user_id']]['phone'];
             }
         }
         $result['numbers'] = $numbers;
