04bf6f62e07cef11decdf70cf573fbf8ad4babc0
una <by2live@hotmail.com>
2018-03-06 15:46:22 +0800

    完成邀请连接

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,6 @@
 <?php
 // 邀请页面
 Route::get('/invite/{phone}', 'UserController@invitePage')->where('phone', '^1[3456789]{1}\d{9}$');
+
+// 邀请页面2
+Route::get('/invite2/{phone}', 'UserController@invitePage');
